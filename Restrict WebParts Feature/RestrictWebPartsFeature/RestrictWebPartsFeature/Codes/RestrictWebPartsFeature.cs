69b66904c3ab4e06fb73092307f1e26262c1034a
madhur_ahuja <none@none.com>
2008-10-28 14:22:02 +0000

    Initial check-in

---
+++ Restrict WebParts Feature/RestrictWebPartsFeature/RestrictWebPartsFeature/Codes/RestrictWebPartsFeature.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Administration;
+
+namespace RestrictWebPartsFeature
+{
+    class RestrictWebPartsFeature:Microsoft.SharePoint.WebControls.LayoutsPageBase
+    {
+        protected override void CreateChildControls()
+        {
+            
+            base.CreateChildControls();
+        }
+    }
+}
---
+++ Restrict WebParts Feature/RestrictWebPartsFeature/RestrictWebPartsFeature/RestrictWebPartsFeature/RestrictWp.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Assembly Name="RestrictWebPartsFeature, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null" %>
+<%@ Page Language="C#" Inherits="RestrictWebPartsFeature" Title="Restrict WebParts" MasterPageFile="_layouts/application.master" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderId="PlaceHolderMain" runat="server">
+
+</asp:Content>
