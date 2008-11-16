3d11117778ee76eaa04ad39e6cf3c0724b082d69
madhur_ahuja <none@none.com>
2008-11-16 05:39:22 +0000

    Added SiteQuota Webpart

---
+++ SiteQuotaWebPart/SiteQuotaWebPart/SiteQuotaWebPart.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.SharePoint;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI;
+using System.Xml;
+using System.Xml.Xsl;
+using System.Xml.XPath;
+using System.IO;
+namespace SiteQuotaWebPart
+{
+    public class SiteQuotaWebPart:WebPart
+    {
+        [Personalizable(PersonalizationScope.Shared)]
+        public string XslText
+        {
+            get;
+            set;
+        }
+
+
+        protected override void CreateChildControls()
+        {
+            int quotaUse = 0;
+
+            SPSite mySite = SPContext.Current.Site;
+            long maxLevel=0;
+            long usage=0;
+            SPSecurity.RunWithElevatedPrivileges(delegate
+            {
+                using (SPSite elevatedSite = new SPSite(mySite.ID))
+                {
+                    maxLevel = elevatedSite.Quota.StorageMaximumLevel;
+                    usage = elevatedSite.Usage.Storage;
+
+                    if (maxLevel != 0)
+                    {
+                        quotaUse = Convert.ToInt32(usage * 100 / maxLevel);
+                    }
+
+
+
+                }
+            });
+
+            //MemoryStream ms = new MemoryStream();
+            //StringBuilder sb=new StringBuilder();
+            StringWriter sw = new StringWriter();
+            XmlTextWriter textWriter = new XmlTextWriter(sw);
+            textWriter.WriteStartDocument();
+            textWriter.WriteStartElement("SPWeb");
+            textWriter.WriteElementString("StorageMaximumLeveL", maxLevel.ToString());
+            textWriter.WriteElementString("Storage", usage.ToString());
+            textWriter.WriteEndDocument();
+            textWriter.Close();
+
+
+          
+
+            XmlDocument xmldoc = new XmlDocument();
+            xmldoc.LoadXml(sw.ToString());
+
+            XslCompiledTransform xmlTransform = new XslCompiledTransform();
+            Stream xsltStream=this.GetType().Assembly.GetManifestResourceStream("SiteQuotaWebPart.Default.xslt");
+
+            XPathDocument xPathDoc = new XPathDocument(xsltStream);
+
+            
+            xmlTransform.Load(xPathDoc);
+            MemoryStream ms=new MemoryStream();
+            StringWriter sww=new StringWriter();
+            XmlTextWriter xw=new XmlTextWriter(sww);
+            xmlTransform.Transform(xmldoc, xw);
+
+            this.Controls.Add(new LiteralControl(sww.ToString()));
+            this.SetPersonalizationDirty();
+            //textWriter.WriteString(maxLevel.ToString());
+            
+
+            base.CreateChildControls();
+        }
+
+        public override EditorPartCollection CreateEditorParts()
+        {
+            List<EditorPart> editors = new List<EditorPart>();
+            XslEditor xsl = new XslEditor();
+            xsl.Title = "Edit Xsl";
+            xsl.ID = "xslid";
+            editors.Add(xsl);
+
+            return new EditorPartCollection(editors);
+            //return base.CreateEditorParts();
+        }
+
+        
+    }
+}
---
+++ SiteQuotaWebPart/SiteQuotaWebPart/XslEditor.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.SharePoint;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI;
+using System.Xml;
+using System.Web.UI.WebControls;
+
+namespace SiteQuotaWebPart
+{
+    public class XslEditor : EditorPart
+    {
+        private TextBox _xslText;
+      //  private Button _xslButton;
+
+        public override bool ApplyChanges()
+        {
+            SiteQuotaWebPart quotaWebPart = this.WebPartToEdit as SiteQuotaWebPart;
+
+            if (quotaWebPart != null)
+            {
+                this.EnsureChildControls();
+                quotaWebPart.XslText = _xslText.Text;
+            }
+
+            return (quotaWebPart != null);
+        }
+
+        public override void SyncChanges()
+        {
+            SiteQuotaWebPart quotaWebPart = this.WebPartToEdit as SiteQuotaWebPart;
+            if (quotaWebPart != null)
+            {
+                this.EnsureChildControls();
+                _xslText.Text = quotaWebPart.XslText;
+            }
+        }
+
+        protected override void CreateChildControls()
+        {
+            this._xslText = new TextBox();
+            _xslText.TextMode = TextBoxMode.MultiLine;
+            _xslText.Height = Unit.Pixel(200);
+            this.Controls.Add(_xslText);
+
+            
+            base.CreateChildControls();
+        }
+
+        
+ 
+
+    } 
+}
